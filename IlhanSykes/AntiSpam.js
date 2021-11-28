9a9fe7f91350814b3f8b5e1af08955023910579d
Fbotzz <91331515+Fbotzz@users.noreply.github.com>
2021-11-28 17:13:46 +0700

    Update AntiSpam.js

--- IlhanSykes/AntiSpam.js
+++ IlhanSykes/AntiSpam.js
@@ -9,7 +9,7 @@ const addFilter = (from) => {
     usedCommandRecently.add(from);
     setTimeout(() => {
         return usedCommandRecently.delete(from);
-    }, 1000); // INI WAKTU JEDA SPAM NYA, DEFAULT 2 DETIK!
+    }, 6000); // INI WAKTU JEDA SPAM NYA, DEFAULT 2 DETIK!
 };
 module.exports = {
     msgFilter: {
